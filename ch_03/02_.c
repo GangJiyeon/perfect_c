339758ccbbeb9b1c393ec1d4cad69a4a34d5225f
GangJiyeon <wldus23966@naver.com>
2024-01-17 20:16:26 +0900

    Feat: variables and printf

---
+++ ch_03/02_.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //변수 선언
+    int credit;
+
+    //변수 초기화
+    credit = 15;
+
+
+    //변수 선언 및 초기화
+    int year = 2024;
+
+
+    printf("%d년도 \n", year);
+    printf("이수학점: %d학점", credit);
+    
+
+    return 0;
+}
