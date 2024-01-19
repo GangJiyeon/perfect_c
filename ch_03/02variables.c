377dcd72cd44a9610ff8932ee4d0dd834ca6a596
GangJiyeon <wldus23966@naver.com>
2024-01-19 13:37:46 +0900

    Feat: review variables

---
+++ ch_03/02variables.c
@@ -0,0 +1,17 @@
+/*
+    perfect_c / ch-03 / 02variables.c
+*/
+
+#include <stdio.h>
+int main(void)
+{
+    int year = 2022;    //선언 + 초기화
+
+    int credit;         //선언
+    credit = 15;        //초기화
+
+    printf("%d년도 \n", year);
+    printf("이수학점: %d\n", credit);
+
+    return 0;
+}
