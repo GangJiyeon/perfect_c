1939be8979689c8bc9d8c3c5c064cb7f4b850f83
GangJiyeon <wldus23966@naver.com>
2024-01-19 13:26:48 +0900

    Feat: review commets

---
+++ ch_03/01comments.c
@@ -0,0 +1,17 @@
+/*
+    perfect_c / ch_03 / 01comments.c
+    c 프로그램의 기초를 다지기 위한 주석, 문장, 키워드 이해
+    v 1.0 | 2024.01.19 jiyeon
+*/
+#include <stdio.h>
+
+// 운영체제가 호출하는 함수, void로 매개변수가 없음을 표시
+int main(void)
+{
+    puts("3장 첫 C프로그램!\n");
+
+    printf("키워드: int void return 등\n");
+    printf("식별자: main puts printf 등\n");
+    
+    return 0;
+}
