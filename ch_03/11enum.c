93c00a9fa4f18a823959c7cb3379cd1e85601cc8
GangJiyeon <wldus23966@naver.com>
2024-01-23 00:16:04 +0900

    Feat: study more enum

--- ch_03/11enum.c
+++ ch_03/11enum.c
@@ -3,9 +3,15 @@
 int main(void)
 {
     //열거형 상수
-
     enum DAY {SUN, MON, TUE, WED, THU, FRI, SAT};
 
-    printf("sunday : %d", SUN);
+    printf("sunday : %d\n", SUN);
+
+
+    //값 지정하기
+    enum SHAPE {POINT, LINE, TRI=3};
+    printf("TRI= %d", TRI);
+
+    
     return 0;
 }
