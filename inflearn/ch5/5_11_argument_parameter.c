0d4b5454ee128e2c7f682ca0ff7b8ef2f63028e9
GangJiyeon <wldus23966@naver.com>
2024-04-04 16:43:47 +0900

    arguments and parameters study

---
+++ inflearn/ch5/5_11_argument_parameter.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+//ansi function prototype declaration
+void draw(int n);
+
+int main()
+{
+    int i = 5;
+    char c = '#';   //35
+    float f = 7.1f;
+
+    draw(i);
+    draw((int)c);
+    draw((int)f);
+
+    return 0;
+
+}
+
+/*arguments vs parameters*/
+//arguments => values
+//parameters => variables
+
+void draw(int n)
+{
+    while (n-- > 0)
+    {
+        printf("*");
+    }
+
+    printf("\n");   
+}
+
+
