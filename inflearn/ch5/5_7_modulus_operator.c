c6660e9c32d691f12780fd4ec0d02546a6e23dae
GangJiyeon <wldus23966@naver.com>
2024-04-04 15:52:13 +0900

    modulus more

--- inflearn/ch5/5_7_modulus_operator.c
+++ inflearn/ch5/5_7_modulus_operator.c
@@ -11,14 +11,39 @@ int main()
     printf("input seconds: ");
     scanf("%d", &seconds);
 
-    minutes = seconds / 60;
-    seconds = seconds % 60;
+    while (seconds >= 0)
+    {
+        minutes = seconds / 60;
+        seconds = seconds % 60;
 
-    hours = minutes / 60;
-    minutes %= 60;
+        hours = minutes / 60;
+        minutes %= 60;
+        printf("%d hours, %d minutes, %d seconds\n", hours, minutes, seconds);
+
+        printf("input seconds: ");
+        scanf("%d", &seconds);
+    }
     
-    printf("%d hours, %d minutes, %d seconds\n", hours, minutes, seconds);
+
     printf("good bye");
+
+    int div, mod;
+
+    div = 11 / 5;
+    mod = 11 % 5;
+    printf("div = %d, mod = %d\n ", div, mod);  // 2, 1
+
+    div = 11 / -5;
+    mod = 11 % -5;
+    printf("div = %d, mod = %d\n ", div, mod);  // -2, 1
+
+    div = -11 / -5;
+    mod = -11 % -5;
+    printf("div = %d, mod = %d\n ", div, mod);  // 2, -1
     
+    div = -11 / 5;
+    mod = -11 % 5;
+    printf("div = %d, mod = %d\n ", div, mod);  // -2, -1
+
     return 0;
 }
