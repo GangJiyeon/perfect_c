5e508bc9d63aeda7336780391cb85ee3d5ad20e3
GangJiyeon <wldus23966@naver.com>
2024-04-08 19:41:33 +0900

    true(!=0) and false(=0)

---
+++ inflearn/ch6/6_5_true_false.c
@@ -0,0 +1,19 @@
+/*
+6.5. 사실과 거짓
+*/
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+
+    int tv, fv;
+    tv = (1 < 2);
+    fv = (1 > 2);
+
+    printf("true is %d\n", tv);
+    printf("true is %d\n", fv);
+
+    // 0이면 false고 나머지는 다 true이다
+    return 0;
+}
