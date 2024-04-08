53f715c44eeeedd92e7d2d41785a6854995b6a47
GangJiyeon <wldus23966@naver.com>
2024-04-08 19:33:02 +0900

    relational operators

---
+++ inflearn/ch6/6_4_relational_operators.c
@@ -0,0 +1,50 @@
+/*
+6.4. 관계 연산자(relational operators)
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+#include <math.h>   //fabs()
+
+int main()
+{
+
+    /*======= relational operators =======  
+    |   <       is less than   
+    |   <=      is less than or equal to
+    |   ==      is equal to
+    |   >=      is greater thant or equal to
+    |   >       is greater than
+    |   !=      is not equal to
+    ======================================*/
+
+
+    int n = 0;
+    while (n++ < 5)
+        printf("%d", n);
+
+    printf("\n");
+
+    char c = 'A';
+    while(c != 'Z')
+        printf("%c", c++);
+
+    printf("\n");
+    const double PI = 3.1415926525897932;   //최근 프로그램에서는 define 보다 const 권장
+    double guess = 0.0;
+
+    printf("\n");
+    printf("input pi: ");
+
+    scanf("%lf", &guess);
+    
+    //while(guess != PI)
+    while(fabs(guess - PI) > 0.01)
+    {
+        printf("fool~~~! try again ><\n");
+        scanf("%lf", &guess);
+    }
+
+    printf("goo~~~D");
+    return 0;
+}
