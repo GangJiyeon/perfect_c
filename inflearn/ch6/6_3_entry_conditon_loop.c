23ae6c3754c5b39c3ec61ef75dea43e691013b63
GangJiyeon <wldus23966@naver.com>
2024-04-08 19:18:02 +0900

    entry condition loop

---
+++ inflearn/ch6/6_3_entry_conditon_loop.c
@@ -0,0 +1,56 @@
+/*
+6.3. 진입조건 루프 while (Entry-Condition Loop)
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+    /*
+    while (expression)  => expression이 0이면 false, 실행 중지
+        statement
+    */
+
+    int i;
+
+    /*
+    i = 1;
+    while(i < 5)     //infinite loop
+        printf("hi\n"); //iteration
+
+    */
+
+    /*
+    i = 1;
+    while(--i < 5)     //wrong direction
+        printf("hi\n"); 
+
+    */
+        
+    /*
+    i = 1;
+    while(i < 5)     
+    {
+        printf("i before = %d", i);
+        i++;
+        printf("i after = %d\n", i);
+    }
+    */
+
+    /*
+    i = 10;
+    while(i++ < 5)     //cannot enter
+    {
+        printf("hi"\n);
+    }
+    */
+
+    /*
+    i = 0;
+    while(i < 5)     //{}
+        printf("%i\n", i);
+        i++;    //indenting
+    */
+
+}
