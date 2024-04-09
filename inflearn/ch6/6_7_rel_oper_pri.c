5f1af89487f9811a1f7e13ab7a1d5f40d2adea2f
GangJiyeon <wldus23966@naver.com>
2024-04-09 11:58:41 +0900

    relational operators precendence

---
+++ inflearn/ch6/6_7_rel_oper_pri.c
@@ -0,0 +1,26 @@
+/*
+6.7. 관계연산자의 우선순위
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+
+    int x = 1, y = 2, z;
+
+    x > y + 2;
+    x > (y + 2);
+
+    x = y > 2;
+    x = (y > 2);
+
+    z = x > y;
+    z = (x > y);
+
+    x != y == z;    //우선순위가 같음 => left to right
+    (x != y) == z;
+
+    return 0;
+}
