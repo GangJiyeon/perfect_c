83b2c92306f7e3d7c84756bbb645d6f214ce1f93
GangJiyeon <wldus23966@naver.com>
2024-04-09 13:50:49 +0900

    zonos paradoxes example

---
+++ inflearn/ch6/6_12_zonos_paradoxes.c
@@ -0,0 +1,41 @@
+/*
+6.12. 제논의 역설 시뮬레이션 예제
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+    /*
+        simulation of a moving object
+
+        - speed = 1.0 m/s
+        - dt = 1.0s
+        - distance traveled during dt = speed * dt
+
+        - step 1 : 1.0 m/s * 1.0 s = 1.0 m, dist = 1.0 m
+        - step 2 : 1.0 m/s * 1.0 s = 1.0 m, dist = 1.0 m + 1.0 m = 2.0 m
+        - step 3 : 1.0 m/s * 1.0 s = 1.0 m, dist = 2.0 m + 1.0 m = 3.0 m
+        - ......
+    */
+
+    const double speed = 1.0;
+    const unsigned repeat_max = 10;
+
+    double dt = 1.0;
+    double time = 0.0;  //elapsed time
+    double dist = 0.0;  //distance
+
+    for(unsigned i = 0; i < repeat_max; ++i)
+    {
+        dist += speed * dt;
+        time += dt;
+
+        printf("elapsed time = %.10fs, distance = %.10fm\n", time, dist);
+
+        dt *= 0.5;  //나누기보다 곱하기가 더 빠르다
+    }
+
+
+}
