77fcdf88079250d97ea0f8b72c2bc9bc59b96705
GangJiyeon <wldus23966@naver.com>
2024-04-09 13:19:01 +0900

    various assignment operator

---
+++ inflearn/ch6/6_10_various_assignment_operator.c
@@ -0,0 +1,29 @@
+/*
+다양한 대입 연산자들, 어셈블리 코드 확인법
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+
+    /*
+        +=, -=, *=, /=, %=
+    */
+
+    int i = 1024;
+
+    i = i + 10;
+    i += 10;
+
+    i %= 4;
+
+    i = i * (1 + 2);
+    i *= 1 + 2;
+    i *= (1 + 2);   //chech precedence
+
+    for(int i = 0; i < 10; i++)
+        ;
+    return 0;
+}
