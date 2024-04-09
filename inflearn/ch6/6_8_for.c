94332e990f4338d71753659071e3cf7d9c484458
GangJiyeon <wldus23966@naver.com>
2024-04-09 12:12:54 +0900

    for

---
+++ inflearn/ch6/6_8_for.c
@@ -0,0 +1,32 @@
+/*
+6.8. for 루프 소개
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+
+    //indefinite loop vs. counting loop
+    /*
+    while(1)
+    {
+        do somethig
+    }
+    */
+    
+    /*
+    //counting loop
+    1. counter initialization
+    2. couter check
+    3. couter change
+    */
+
+    
+
+    for(int i = 1; i <= 10; i++)
+        printf("%d", i);
+
+    return 0;
+}
