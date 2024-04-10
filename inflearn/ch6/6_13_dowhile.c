32170c087df4ec6d434808d2154efeb63cf2df9c
GangJiyeon <wldus23966@naver.com>
2024-04-10 13:20:46 +0900

    do while

---
+++ inflearn/ch6/6_13_dowhile.c
@@ -0,0 +1,35 @@
+/*
+6.13. do while, exit condition loop
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+    int const secret_code = 612111;
+    int user_num = 0;
+
+    /* mine
+    while(user_num != secret_code)
+    {
+        printf("Enter secret code: ______\b\b\b\b\b\b");
+        scanf("%d", &user_num);
+    }
+
+    printf("Good!");
+
+    */
+
+    /* use do while */
+    do  //일단 해라
+    {
+        printf("Enter secret code: ______\b\b\b\b\b\b");
+        scanf("%d", &user_num);
+    }
+    while(user_num != secret_code); //while동안
+
+    printf("Good!");
+
+    return 0;
+}
