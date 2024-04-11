5ca1e6560623e9875dae6d67db313b63383868a9
GangJiyeon <wldus23966@naver.com>
2024-04-11 13:02:48 +0900

    getchar and putchar

---
+++ inflearn/ch7/7_2_getchar_putchar.c
@@ -0,0 +1,50 @@
+/*
+7.2. 표준 입출력 함수들 => getchar(), putchar()
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+    /*
+        1. introduce getchar(), putchar()
+        2. use while loop to process character sequences
+        3. filter a spcific character
+        4. convert numbers to asterisks
+        5. lower characters to upper characters
+    */
+
+    char ch;
+
+    //buffer에 저장
+    //ch = getchar(); // 문자 -> int(아스키코드) -> 문자
+    //putchar(ch);    // 문자 -> int(아스키코드) -> 문자
+
+    while((ch = getchar()) != '\n')   //use '\n' to find the end of a senetece
+    {
+        if(ch == 'f' || ch == 'F')
+        {
+            ch = 'x';
+        }
+        else if(ch >= '0' && ch <= '9')
+        {
+            ch = '*';
+        }    
+
+        if('A' <= ch && ch <= 'Z')
+        {
+            ch += 32; //('a' - 'A');
+        }
+        else if('a' <= ch && ch <= 'z'){
+            ch -= ('a' - 'A');
+        }
+        
+        putchar(ch);
+        //ch = getchar(); //저장된 걸 가져옴(while을 돌면서 가져오는 것X)
+    };
+
+    putchar(ch);
+
+    return 0;
+}
