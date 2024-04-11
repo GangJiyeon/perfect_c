9e2af8d3dff4ba92a4c01e2e2ba347ba9dd6648c
GangJiyeon <wldus23966@naver.com>
2024-04-11 12:03:16 +0900

    study if

---
+++ inflearn/ch7/7_1_branching_statement.c
@@ -0,0 +1,27 @@
+/*
+7.1. if => 분기문
+
+if(expression) => false(=0)이 아니라면
+    statement
+*/
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+
+    int number;
+
+    printf("input a positive integer : _\b");
+    scanf("%d", &number);
+
+    if(number % 2 == 0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
