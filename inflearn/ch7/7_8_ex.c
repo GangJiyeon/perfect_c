c26480e48c92c227324dd996cdbfc93a7d61dce0
GangJiyeon <wldus23966@naver.com>
2024-04-14 19:01:18 +0900

    study flag

--- inflearn/ch7/7_8_ex.c
+++ inflearn/ch7/7_8_ex.c
@@ -3,6 +3,8 @@
 */
 #define _CRT_SECURE_NO_WARNING
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 #define PERIOD '.'
 
 int main()
@@ -15,16 +17,37 @@ int main()
     int char_num = 0;
     int word_num = 1;
     int line_num = 1;
+    bool word_flag = false; //new word
+    bool line_flag = false; //new line
 
     while((ch = getchar()) != PERIOD)
     {
-        ++char_num;
+        if(!isspace(ch))
+            ++char_num;
 
+        //글자가 입력되었는데, 새로운 줄(flag = false)이라면
+        if(!isspace(ch) && !line_flag)
+        {
+            //줄 수 추가, line_flag = false;
+            line_num++;
+            line_flag = true;
+        }
+
+        //줄바꿈이 일어나면 flag = false
         if(ch == '\n')
-            ++line_num;
+        {
+            line_flag = false;
+        }    
 
-        if(ch == ' ')
+        if(!isspace(ch) && !word_flag)
+        {
             ++word_num;
+            word_flag = true;
+        }
+
+        if(isspace(ch))
+            word_flag = false;
+            
     }
 
     printf("Characters = %d, Words = %d, Lines = %d", char_num, word_num, line_num);
