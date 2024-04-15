cd5bbc243d560afe91d4281dac1cf8bc65b43692
GangJiyeon <wldus23966@naver.com>
2024-04-15 12:07:07 +0900

    buffer and _getche &  _getch

---
+++ inflearn/ch8/8_1_buffer.c
@@ -0,0 +1,30 @@
+/*
+8.1. 입출력 버퍼
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+#include <conio.h> //windows, _getch(), _getche()
+
+int main()
+{
+
+    char c;
+     
+    /*
+    
+    */ 
+   
+    //버퍼를 사용하지 않음
+    while((c = _getche()) != '.')   //echo O
+    {
+        putchar(c);
+    } 
+
+    while((c = _getch()) != '.')   //echo X
+    {
+        putchar(c);
+    }
+
+    return 0;
+}
