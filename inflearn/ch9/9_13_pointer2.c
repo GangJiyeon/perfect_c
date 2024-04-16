dbd0eb769eb85457ccc32dea1b463cb12f0d4033
GangJiyeon <wldus23966@naver.com>
2024-04-16 23:47:49 +0900

    pointer

---
+++ inflearn/ch9/9_13_pointer2.c
@@ -0,0 +1,21 @@
+/*
+9.13. 포인터의 기본적인 사용방법
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+    int a = 123, b;
+    int *a_ptr;
+    a_ptr =  &a;
+
+    printf("%d %d %p", a, *a_ptr, a_ptr);
+
+    *a_ptr = 456;
+    printf("%d %d %p", a, *a_ptr, a_ptr);
+
+    b = a_ptr;  //가능은 하지만 권장XXXXXX!!!!
+    return 0;
+}
