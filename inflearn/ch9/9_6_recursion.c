57fbf0b4baf9cb0a871a52a2e5920108082281be
GangJiyeon <wldus23966@naver.com>
2024-04-16 14:38:17 +0900

    recursion

---
+++ inflearn/ch9/9_6_recursion.c
@@ -0,0 +1,27 @@
+/*
+9.6. 재귀호출 = 재귀함수
+무한루프에서 사용하면 안 됨
+*/
+
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+void my_func(int);
+
+int main()
+{
+    my_func(1);
+
+    return 0;
+}
+
+void my_func(int n)
+{
+    printf("level %d, address of variable n = %p\n", n, &n);
+
+    //stop condition을 만나기 전까지는 함수가 호출되기만 한다
+    if(n < 10)
+        my_func(n + 1);
+
+    printf("level %d, address of variable n = %p\n", n, &n);
+}
